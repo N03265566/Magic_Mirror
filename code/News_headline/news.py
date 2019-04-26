ab940d4a889dbf4aaf1d906f0f1d3e9224128b55
N03265566 <bryeng57@gmail.com>
2019-04-26 00:30:30 -0400

    Takes the headlines from the moments tab from twitter and displays them.

---
+++ code/News_headline/news.py
@@ -0,0 +1,14 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+quote_page = 'https://twitter.com/i/moments'
+
+page = urllib2.urlopen(quote_page)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+news_headline = soup.find_all('div', class_='MomentCapsuleSummary-details')
+
+for news_headlines in news_headline:
+	for tag in news_headlines.find_all('a'):
+		print(tag['title'])
